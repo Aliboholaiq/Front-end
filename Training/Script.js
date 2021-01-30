5a02a9a4799bb24655ecec5d0ea847e8dc03beea
iAli3lool <iAli3lool@users.noreply.github.com>
2021-01-30 13:50:29 +0300

    2

--- Training/Script.js
+++ Training/Script.js
@@ -1,3 +1,35 @@
+//node JS server
+/*
+const http = require("http");
+const hostname = "localhost";
+const port = 8000;
+const myServer = http.createServer(function(request,response){
+    response.writeHead(200, {'Content-Type':'text/html' });
+    response.write("Helooooo");
+    console.log(request.url);
+    response.end();
+});
+myServer.listen(port,hostname);
+*/
+
+//node Js express server
+const express = require("express");
+const port = 8000;
+const app = express();
+app.get('/', function(request,respond){
+    respond.send("Hello fanday")
+
+})
+app.get('/aboutus', function(request,respond){
+    respond.send("Hello abouut us")
+
+})
+app.get('/qamar/:qamarnmber', function(request,respond){
+    respond.send("accessing qamarnmber is "+request.params.qamarnmber)
+
+})
+app.listen(port);
+/*
 //change p3 to p1
 function byTagNames(){
     let tagNames=document.getElementsByTagName("p")
@@ -13,7 +45,38 @@ function changeStyle(p){
     document.getElementById("p1").style.fontWeight = "bold";
     document.getElementById(p).style.color = document.getElementById("p1").style.color;
 }
-
+Vue.use(VueResource);
+/////////////// VueJs //////////////
+let recipes = ["1", "2", "3", "4", "5"];
+let app = new Vue({
+    el: '#app',
+    data: {
+        recipes:recipes,
+        empName:'',
+        empWeight:'',
+        allWeights:[],
+        emp:''
+    },
+    computed: {    
+        isDisabled: function(){
+            if((this.empWeight<20)||(this.empName.length<2)){
+                return true
+            }
+            else{
+                return false
+            }
+        }
+    },
+    method: {
+        getData:function(){
+            this.shttp.get("http://localhost:8000/getallrecords")
+            .then(Response=> {
+                this.allWeights = Response.data;
+            })
+        }
+        
+    }
+});
 /////////////// JQuery //////////////
 
 // Java Script
@@ -22,7 +85,6 @@ function changeStyle(p){
 //});
 // Same code in JQuery
 //$(alert('document loaded')) 
-
 $(document).ready(function(){
     //style $("[class='sizes']").css({"color":"brown", "font-weight":"bolder"});
     //all p items $("p").css({"color":"brown", "font-weight":"bolder"});
@@ -43,8 +105,6 @@ $(document).ready(function(){
         }
         
     });
-
-
     $("#p1").on({
         mouseenter:function(){
             console.log("You have entered p1");
@@ -53,4 +113,4 @@ $(document).ready(function(){
             console.log("You have left p1");
         }
     })
-});
+});*/
